5247533c98761e4fffea630d880d8a62d7e2b0b9
POLINA_D <110941588+Monssssster@users.noreply.github.com>
2024-02-06 13:45:52 +0300

    WASDJump 1.0

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float _fallVelocity = 0;
+    
+    public float gravity = 9.8f;
+
+    public float  jumpForce;
+    public float speed;
+
+    private Vector3 _moveVector;
+
+
+    private CharacterController _characterController;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update(){
+
+        _moveVector = Vector3.zero;
+
+        //W
+        if(Input.GetKey(KeyCode.W)){
+            _moveVector += transform.forward;
+        }
+
+        //A
+        if(Input.GetKey(KeyCode.A)){
+            _moveVector -= transform.right;
+        }
+
+        //S
+        if(Input.GetKey(KeyCode.S)){
+            _moveVector -= transform.forward;
+        }
+
+        //D
+        if(Input.GetKey(KeyCode.D)){
+            _moveVector += transform.right;
+        }                           
+
+        //Jump
+        if(Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded){
+            _fallVelocity = -jumpForce;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        _characterController.Move( _moveVector * speed * Time.fixedDeltaTime);
+
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+         _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+         if(_characterController.isGrounded){
+            _fallVelocity = 0;
+         }
     }
 }
