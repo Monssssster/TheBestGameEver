5f16e0cb439675ef7c3c77b3e6fa26e33422aab7
POLINA_D <110941588+Monssssster@users.noreply.github.com>
2024-02-13 17:04:08 +0300

    ZadaniaCubeMiddle&Anim

---
+++ Assets/ZadCubeAnim/CubeAnim.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeAnim : MonoBehaviour
+{
+    [Range(0f,1f)]
+    public float value;
+
+    public Transform cube1;
+    public Transform cube2;
+
+    public Material material1;
+    public Material material2;
+
+    public Material material3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(cube1.position, cube2.position, value);
+        transform.localScale = Vector3.Lerp(cube1.localScale, cube2.localScale, value);
+        material3.color = Color.Lerp(material1.color, material2.color, value);
+    }
+}
---
+++ Assets/ZadCubeMiddle/MiddleCube.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MiddleCube : MonoBehaviour
+{
+
+    public Transform Point1;
+    public Transform Point2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(Point1.position, Point2.position, 0.5f);
+    }
+}
